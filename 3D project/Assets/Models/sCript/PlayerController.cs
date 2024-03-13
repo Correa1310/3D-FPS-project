b927b5f2b40d4e04fd01de4d333bbc9b609a9f43
Correa1310 <146136159+Correa1310@users.noreply.github.com>
2024-03-13 13:33:45 -0700

    Start adding wepons

---
+++ 3D project/Assets/Models/sCript/PlayerController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+     public float moveSpeed = 1f;
+
+     public float jumpForce = 10f;
+    public float mouseSensitivity = 1f;
+    public Transform theCamera;
+    private Vector3 _moveInput;
+    public LayerMask whatIsGround;
+    private bool _canPlayerJump;
+    public Transform groundCheckpoint;
+    private CharacterController _characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    float yVelocity = _moveInput.y;
+
+     
+        //Player movement
+        //_moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        //_moveInput.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        Vector3 forwardDirection = transform.forward * Input.GetAxis("Vertical");
+         Vector3 horizontalDirection = transform.right * Input.GetAxis("Horizontal");
+
+         _moveInput =(forwardDirection + horizontalDirection).normalized;
+         _moveInput *= moveSpeed;
+
+
+         //Apply Jumping
+         _moveInput.y yVelocity;
+     
+        _moveInput.y += Physics.gravity.y * gravityModifier *Time.deltaTime;
+
+        if(_characterController.isGrounded)
+        {
+            _moveInput.y = Physics.gravity.y * gravityModifier * Time.deltaTime;
+        }
+         _canPlayerJump = 
+
+        _characterController.Move(_moveInput * Time.deltaTime);
+
+         //Camera rotation
+
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+
+        theCamera.rotation = Quaternion.Euler(theCamera.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
+    }
+}
