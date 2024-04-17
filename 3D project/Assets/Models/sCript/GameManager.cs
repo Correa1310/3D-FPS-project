82707f4c8635a9ea5a27499a07a221aa257b0181
Correa1310 <146136159+Correa1310@users.noreply.github.com>
2024-04-17 12:51:46 -0700

    Update

--- 3D project/Assets/Models/sCript/GameManager.cs
+++ 3D project/Assets/Models/sCript/GameManager.cs
@@ -2,20 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public TextMeshProUGUI targetText;
 
     private int _targetAmount;
+
+    public winScene;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        GameObject.Find("Game Manager").GetComponent<Timer>();
         Cursor.lockState = CursorLockMode.Locked;
         int floatingTarget = GameObject.FindGameObjectsWithTag("TargetFloating").Length;
         int standingTarget = GameObject.FindGameObjectsWithTag("TargetStanding").Length;
         _targetAmount = floatingTarget + standingTarget;
         targetText.text = "Targets: " + _targetAmount.ToString();
+    }
+
+    void Update()
+    {
+        if(_timer.GetTimeRemaining)
+        SceneManager.LoadScene(loseScene);
     }
 
     // Update is called once per frame
---
+++ 3D project/Assets/Models/sCript/Menus.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menus : MonoBehaviour
+{
+    public int levelOne;
+
+    public void OnPlayButtonPressed()
+    {
+        SceneManager.LoadScene(levelOne);
+    }
+
+    public void OnPlayButtonReleased()
+    {
+        Application.Quit();
+    }
+
+}
